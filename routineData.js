4a27cf367a88c45d8039c001ff5ed5a7aff7c8c6
JDVasquez26 <114019007+JDVasquez26@users.noreply.github.com>
2023-04-24 17:02:11 -0400

    adding data for personal exercise routine

---
+++ routineData.js
@@ -0,0 +1,375 @@
+export const sunday = [
+    {
+        id: 1,
+        name: "flexion leg sit up (bent knee)",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 2,
+        name: "reverse crunch",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 3,
+        name: "flutter kicks",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 4,
+        name: "front plank with twist",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 5,
+        name: "flexion leg sit up (straight arm)",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 6,
+        name: "cross body crunch",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 7,
+        name: "lying leg raise flat bench",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 8,
+        name: "inchworm",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 9,
+        name: "bridge - mountain climber (cross body)",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 10,
+        name: "russian twist",
+        sets: 3,
+        reps: 10
+    },
+]
+
+export const monday = [
+    {
+        id: 1,
+        name: "forward lunge (male)",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 2,
+        name: "walking high knees lunge",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 3,
+        name: "mountain climber",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 4,
+        name: "bodyweight drop jump squat",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 5,
+        name: "lunge with jump",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 6,
+        name: "dumbbell goblet squat",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 7,
+        name: "quads",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 8,
+        name: "split squats",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 9,
+        name:"bench hip extension",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 10,
+        name: "side bridge hip abduction",
+        sets: 3,
+        reps: 10
+    },
+]
+
+/// need to double check these exercises
+//glutes
+export const tuesday = [
+    {
+        id: 1,
+        name: "bodyweight drop jump squat",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 2,
+        name: "burpee",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 3,
+        name: "donkey kicks", //<--
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 4,
+        name:"low glute bridge on floor",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 5,
+        name: "jump knee tucks", //<--
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 6,
+        name: "fly steps",  //<--
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 7,
+        name: "one leg bridge",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 8,
+        name: "pelvic tilt into bridge",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 9,
+        name:"glute bridge march",
+        sets: 3,
+        reps: 10
+    },
+]
+
+
+//triceps
+export const wednesday = [
+    {
+        id: 1,
+        name: "close grip pushups",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 2,
+        name: "tricep dips",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 3,
+        name: "tricep extensions",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 4,
+        name: "get ups",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 5,
+        name: "punches",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 6,
+        name: "side to side chops",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 7,
+        name: "one leg bridge",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 8,
+        name: "hip thrusts",
+        sets: 3,
+        reps: 10
+    },
+]
+
+//biceps
+export const thursday = [
+    {
+        id: 1,
+        name: "leg curls",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 2,
+        name: "hiking pack curls",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 3,
+        name: "door frame rows",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 4,
+        name: "body rows",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 5,
+        name: "sitting pull ups",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 6,
+        name: "pseudo planche",
+        sets: 3,
+        reps: 10
+    },
+]
+
+//BACK
+export const friday = [
+    {
+        id: 1,
+        name: "elbows lifts",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 2,
+        name: "superman",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 3,
+        name: "2MIN STAR PLANK",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 4,
+        name: "alt arm/leg plank",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 5,
+        name: "full arch",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 6,
+        name: "pull up alternatives ",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 7,
+        name: "one leg bridge",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 8,
+        name: "hip thrusts",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 9,
+        name:"one leg hip thrusts",
+        sets: 3,
+        reps: 10
+    },
+]
+
+//chest
+export const saturday = [
+    {
+        id: 1,
+        name: "push ups ",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 2,
+        name: "plank rotations",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 3,
+        name: "chest squares",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 4,
+        name: "shoulder press",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 5,
+        name: "shoulder taps",
+        sets: 3,
+        reps: 10
+    },
+    {
+        id: 6,
+        name: "clapping push ups",
+        sets: 3,
+        reps: 10
+    }
+]
